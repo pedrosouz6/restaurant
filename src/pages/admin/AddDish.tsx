b8be4f8b413028b74f23df5d7a517c7e6d159f42
Pedro Souza0 <peedrosouz6@gmail.com>
2022-03-20 21:57:07 -0300

    update

--- src/pages/admin/AddDish.tsx
+++ src/pages/admin/AddDish.tsx
@@ -2,11 +2,47 @@ import { FaTrash } from 'react-icons/fa';
 import { GrUpdate } from 'react-icons/gr';
 import { IoMdSettings } from 'react-icons/io';
 
+import { useState, FormEvent } from 'react';
+import Axios from 'axios';
+
 import Header from '../../components/HeaderAdmin';
 
 import '../../styles/Admin/addDish.scss';
 
 export default function AddDish() {
+
+    const [ nameDish, setNameDish ] = useState('');
+    const [ ingredients, setIngredients ] = useState('');
+    const [ price, setPrice ] = useState('');
+    const [ note, setNote ] = useState('');
+
+    const [ message, setMessage ] = useState('');
+
+    const sendAddDish = (e: FormEvent) => {
+        e.preventDefault();
+        const validate = nameDish.trim() === '' || ingredients.trim() === '' || price.trim() === '';
+
+        if(validate) {
+            return console.log('campo vazio(add dish)');
+        }
+
+        Axios.post('http://localhost:3333/add/dish', {
+            nameDish,
+            ingredients,
+            price,
+            note
+        })
+        .then(response => {
+            if(response.data.message) {
+                return setMessage(response.data.message);
+            }
+
+            return console.log('erro ao cadastrar um prato')
+        })
+    }
+
+    console.log(message)
+
     return (
         <section id='add-dish'>
             <Header />
@@ -18,12 +54,29 @@ export default function AddDish() {
                     </div>
                     <div className='center'>
                         <div className="form-dish">
-                            <form>
-                                <input id='name-dish' type='text' placeholder='Nome do prato' />
-                                <textarea id='ingredients' placeholder='Ingredientes' />
-                                <input id='price-dish' type='number' placeholder='Preço' />
-                                <textarea id='note' placeholder='Observação(opcional)' />
+                            <form onSubmit={sendAddDish}>
+
+                                <input id='name-dish' type='text' 
+                                placeholder='Nome do prato' 
+                                value={nameDish}
+                                onChange={e => setNameDish(e.target.value)} />
+
+                                <textarea id='ingredients' 
+                                placeholder='Ingredientes' 
+                                value={ingredients}
+                                onChange={e => setIngredients(e.target.value)} />
+
+                                <input id='price-dish' type='number' 
+                                placeholder='Preço' 
+                                value={price}
+                                onChange={e => setPrice(e.target.value)} />
+
+                                <textarea id='note' placeholder='Observação(opcional)'
+                                value={note}
+                                onChange={e => setNote(e.target.value)} />
+
                                 <input type="submit" value="Adicionar" />
+
                             </form>
                         </div>
                     </div>
